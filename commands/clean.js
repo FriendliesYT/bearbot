9636cc46dbeb3f1c5345f17919278589bef641e6
FriendliesYT <friendliesyt@gmail.com>
2018-09-20 11:27:39 -0400

    Update clean.js

--- commands/clean.js
+++ commands/clean.js
@@ -11,16 +11,16 @@ let purple = botconfig.purple;
       module.exports.run = async (bot, message, args) => {
 let cleanedEmbed = new Discord.RichEmbed()
 .setColor(green)
-.setTitle("Bear Bot")
+.setTitle("**BEAR BOT**")
 .setDescription(`Cleaned ${args[0]} messages.`);
 let nopermissionEmbed = new Discord.RichEmbed()
 .setColor(green)
-.setTitle("Bear Bot")
+.setTitle("**BEAR BOT**")
 .setDescription(`${message.author.username}, you don't have permission to clean messages.`);
 let cleancommandsyntaxEmbed = new Discord.RichEmbed()
 .setColor(green)
-.setTitle("**Bear Bot**")
-.setDescription(`Usage: !clean <1 to 100>`);
+.setTitle("**BEAR BOT**")
+.setDescription(`*Usage: !clean <1 to 100>*`);
                 if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(nopermissionEmbed);
                 if(!args[0]) return message.channel.send(cleancommandsyntaxEmbed).then(msg => msg.delete(4000));
                 message.channel.bulkDelete(args[0]).then(() => {
