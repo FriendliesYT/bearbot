dc02018126201b605e38ca7cc995cc0409320462
FriendliesYT <friendliesyt@gmail.com>
2018-09-20 11:50:32 -0400

    Update helpadmin.js

--- commands/helpadmin.js
+++ commands/helpadmin.js
@@ -1,10 +1,21 @@
 const Discord = require("discord.js");
 const fs = require("fs");
-
-module.exports.run = async (bot, message, args) => {
-  if(!message.member.hasPermission("ADMINISTRATOR")) {return message.reply("You don't have permission.");}
-  if(!args[0] || args[0 == "help"]) {return message.channel.send({embed: {color: 15158332, title: "Admin Help Menu", description: `!clear\n!shutdown\n!prefix\n!clearconsole`}});}
-    }
+const botconfig = require("../botconfig.json");
+let red = botconfig.red;
+let orange = botconfig.orange;
+let yellow = botconfig.yellow;
+let green = botconfig.green;
+let lightblue = botconfig.lightblue;
+let darkblue = botconfig.darkblue;
+let purple = botconfig.purple;
+      module.exports.run = async (bot, message, args) => {
+            let adminhelpEmbed = new Discord.RichEmbed()
+            .setColor(red)
+            .setTitle("**BEAR BOT**")
+            .setDescription('◇ !clear\n◇ !shutdown\n◇ !prefix\n◇ !clearconsole');
+            if(!message.member.hasPermission("ADMINISTRATOR")) {return message.reply("You don't have permission.");}
+            if(!args[0] || args[0 == "help"]) {return message.channel.send(adminhelpEmbed).then(msg => (msg.delete(10000)));}
+              }
 module.exports.help = {
   name:"helpadmin"
 }
