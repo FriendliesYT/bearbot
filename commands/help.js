86edb230d93189eb816d156a6dd0e5b7d883c045
FriendliesYT <friendliesyt@gmail.com>
2018-09-20 11:43:50 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,13 @@
 const Discord = require("discord.js");
 const fs = require("fs");
+const botconfig = require("../botconfig.json");
+let red = botconfig.red;
+let orange = botconfig.orange;
+let yellow = botconfig.yellow;
+let green = botconfig.green;
+let lightblue = botconfig.lightblue;
+let darkblue = botconfig.darkblue;
+let purple = botconfig.purple;
       module.exports.run = async (bot, message, args) => {
 
         let helpEmbed = new Discord.RichEmbed()
