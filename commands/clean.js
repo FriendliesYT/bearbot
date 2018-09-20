44015999afcd2b9ab7bc7745f9f519f980aa354a
FriendliesYT <friendliesyt@gmail.com>
2018-09-20 11:31:47 -0400

    Update clean.js

--- commands/clean.js
+++ commands/clean.js
@@ -21,7 +21,7 @@ let cleancommandsyntaxEmbed = new Discord.RichEmbed()
 .setColor(green)
 .setTitle("**BEAR BOT**")
 .setDescription(`Usage: !clean <1 to 100>`);
-                if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(nopermissionEmbed);
+                if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(nopermissionEmbed).then(msg => msg.delete(3000));;
                 if(!args[0]) return message.channel.send(cleancommandsyntaxEmbed).then(msg => msg.delete(4000));
                 message.channel.bulkDelete(args[0]).then(() => {
                 message.channel.send(cleanedEmbed).then(msg => msg.delete(3000));});}
